b499cc544297db702115b5bc4f5be46966312bd4
Biplab <10.29.bips@gmail.com>
2026-01-16 16:35:53 +0530

    justify center

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -7,7 +7,7 @@ export function Projects(){
     return (
         <>
             <h1 className="text-center py-[40px] bg-[black] text-[50px] text-[#09e109]" id="projects">PROJECTS.</h1>
-            <div className="w-full px-[10px] flex md:justify-center overflow-x-scroll bg-[black] gap-10 py-[20px]">
+            <div className="w-full px-[10px] flex justify-center md:justify-center overflow-x-scroll bg-[black] gap-10 py-[20px]">
                 <a href="https://bipsweather.netlify.app">
                     <Cards image="/Screen Shot 2026-01-15 at 16.27.54.png" para="A weather app using html css and javascript"/>
                 </a>
