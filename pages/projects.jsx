df053184a28d8a6498f53346d69681dc617ebf53
Biplab <10.29.bips@gmail.com>
2026-01-16 21:59:14 +0530

    new card added

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -7,7 +7,10 @@ export function Projects(){
     return (
         <>
             <h1 className="text-center py-[40px] bg-[black] text-[50px] text-[#09e109]" id="projects">PROJECTS.</h1>
-            <div className="w-full px-[50px] justify-center flex md:justify-center overflow-x-scroll bg-[black] gap-10 py-[20px]">
+            <div className="w-full px-[50px] flex md:justify-center overflow-x-scroll bg-[black] gap-10 py-[20px]">
+                <a href="https://bipscarwah.netlify.app/">
+                    <Cards image="/carwash.png" para="A carwash landing page purely using HTML CSS and Javascript" />
+                </a>
                 <a href="https://bipsweather.netlify.app">
                     <Cards image="/Screen Shot 2026-01-15 at 16.27.54.png" para="A weather app using html css and javascript"/>
                 </a>
