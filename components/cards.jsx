ae319de4ade1921bd1b8cf648df588f745c31a34
Biplab <10.29.bips@gmail.com>
2026-01-16 16:27:14 +0530

    card resized again

--- components/cards.jsx
+++ components/cards.jsx
@@ -5,7 +5,7 @@ export function Cards( {para , image} ){
     return(
         <>
                 <div 
-                className="overflow-y-scroll border-[2px] border-[#0af00a] w-[300px] h-[250px]  md:w-[350px] md:h-[300px] flex-shrink-0 bg-cover bg-center text-[30px] rounded-2xl text-center content-end md:hover:scale-[1.1] transition-all- duration-200 ease-in-out"
+                className="overflow-y-scroll border-[2px] border-[#0af00a] w-[250px] h-[200px]  md:w-[350px] md:h-[300px] flex-shrink-0 bg-cover bg-center text-[30px] rounded-2xl text-center content-end md:hover:scale-[1.1] transition-all- duration-200 ease-in-out"
                 >    
                  <img
                  src={`${image}`}
